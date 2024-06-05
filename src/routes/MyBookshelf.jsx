837763b30dc7c62eef46f2f4c414e4d1ce2f2fe5
Madhav Mishra <60259965+madhav240@users.noreply.github.com>
2024-06-05 20:23:29 +0530

    initial commit

--- src/routes/MyBookshelf.jsx
+++ src/routes/MyBookshelf.jsx
@@ -4,7 +4,7 @@ export default function MyBookshelf() {
   const books = JSON.parse(localStorage.getItem("books")) || [];
   return (
     <>
-      <h1 className="text-center font-bold text-lg"> My Bookshelf</h1>
+      <h1 className="text-center font-bold"> My Bookshelf</h1>
       <div className="mt-8 grid gap-3 sm:grid-cols-2 md:grid-cols-4 ">
         {books.map((book, index) => (
           <BookCard props={{ book, showAddBtn: false }} key={index} />
